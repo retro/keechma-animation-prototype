fdd7d85dd291832b57f754a8f683ce0c457dba09
Mihael Konjevic <konjevic@gmail.com>
2017-10-09 18:17:11 +0200

    Fix frame calculation

--- src/cljs/animation_keechma/controllers.cljs
+++ src/cljs/animation_keechma/controllers.cljs
@@ -11,7 +11,7 @@
 
 (.config Promise #js {:cancellation true})
 (def calc (spring/make-calculator {:tension 40 :friction 7 :overshoot-clamping? false}))
-(def calc-2 (spring/make-calculator {:tension 40 :friction 7}))
+(def calc-2 (spring/make-calculator {:tension 100 :friction 7}))
 
 
 (def anim-path [:kv :anim-state])
@@ -25,7 +25,8 @@
 
 (defn reverse-position [anim-state]
   (when anim-state
-    (update anim-state :position #(.abs js/Math (- % 1)))))
+    (-> anim-state
+        (update :position #(.abs js/Math (- % 1))))))
 
 (defn calculate-frames [{:keys [calculator values]} forward? current-state]
   (let [values (if forward? values (reduce-kv (fn [m k v] (assoc m k (reverse v))) {} values))
@@ -33,24 +34,73 @@
         start-state (if (= forward? (:forward? state)) state (reverse-position state))]
     (vec (spring/calculate-frames calculator values start-state))))
 
-(defn delay-to-frames-count [frames-count delay]
-  (.round js/Math (* frames-count (/ delay 100))))
+(defn add-start-frames [animation frame-count]
+  (let [frames (:frames animation)
+        repeating-frame (assoc-in (first frames) [:keechma.toolbox/anim-state :placeholder?] true)
+        frames-with-delay (into (vec (repeat frame-count repeating-frame)) frames)]
+    (assoc animation :frames frames-with-delay)))
 
-(defn add-start-frames [frames frame-count]
-  (into (vec (repeat frame-count (assoc-in (first frames) [:keechma.toolbox/anim-state :placeholder?] true))) frames))
+(defn add-end-frames [animation frame-count]
+  (let [frames (:frames animation)
+        repeating-frame (assoc-in (last frames) [:keechma.toolbox/anim-state :placeholder?] true)
+        frames-with-delay (into frames (repeat frame-count repeating-frame))]
+    (assoc animation :frames frames-with-delay)))
 
-(defn add-end-frames [frames frame-count]
-  (into frames (repeat frame-count (assoc-in (last frames) [:keechma.toolbox/anim-state :placeholder?] true))))
+(defn calculate-forward-delay [prev-animation delay] 
+  (if (= 0 delay)
+    delay
+    (let [max-idx (dec (count (:frames prev-animation)))]
+      (loop [frame-idx 0]
+        (let [frame-position (get-in prev-animation [:frames frame-idx :keechma.toolbox/anim-state :position])]
+          (if (or (= max-idx frame-idx)
+                  (>= frame-position delay))
+            frame-idx
+            (recur (inc frame-idx))))))))
 
-(defn add-delay [animations forward? max-frames-count]
-  (map (fn [animation]
-         (if-let [delay (:delay animation)]
-           (let [first-position (get-in animation [:frames 0 :keechma.toolbox/anim-state :position])
-                 delay (* (.abs js/Math (- 1 first-position)) delay)]
-             (assoc animation :frames
-                    ((if forward? add-start-frames add-end-frames)
-                     (:frames animation) (delay-to-frames-count delay max-frames-count))))
-           animation)) animations))
+(defn add-forward-delay [animations]
+  (loop [animations animations
+         with-delay []
+         idx 0]
+    (if (empty? animations)
+      with-delay
+      (if (= idx 0)
+        (recur (rest animations)
+               (conj with-delay (first animations))
+               (inc idx))
+        (let [current-animation (first animations)
+              delay (calculate-forward-delay (last with-delay) (/ (or (:delay current-animation) 0) 100))]
+          (recur (rest animations)
+                 (conj with-delay (add-start-frames current-animation delay))
+                 (inc idx)))))))
+
+(defn add-back-delay [animations]
+  (loop [animations animations
+         with-delay []
+         idx 0]
+    (if (empty? animations)
+      with-delay
+      (if (= idx 0)
+        (recur (rest animations)
+               (conj with-delay (first animations))
+               (inc idx))
+        (let [current-animation (first animations)
+              prev-animation (last with-delay)
+              prev-delay (:delay prev-animation)]
+          (if (zero? prev-delay)
+            (recur (rest animations)
+                   (conj with-delay current-animation)
+                   (inc idx))
+            (let [delay (- (count (:frames prev-animation))
+                           (- (count (:frames current-animation))
+                              (calculate-forward-delay current-animation (/ prev-delay 100))))]
+              (recur (rest animations)
+                     (conj with-delay (add-start-frames current-animation delay))
+                     (inc idx)))))))))
+
+(defn add-delay [animations forward?]
+  (if forward?
+    (add-forward-delay animations)
+    (add-back-delay animations)))
 
 (defn normalize-frames [animations forward?]
   (let [max-frames-count (apply max (map #(count (:frames %)) animations))]
@@ -58,27 +108,22 @@
            (let [frames (:frames animation)
                  frames-count (count frames)]
              (if (not= max-frames-count frames-count)
-               (assoc animation :frames
-                      ((if forward? add-end-frames add-start-frames) frames (- max-frames-count frames-count)))
+               (add-end-frames animation (- max-frames-count frames-count)) 
                animation))) animations)))
 
-(defn reverse-animations [animations forward?]
-  (if forward?
-    (vec animations)
-    (vec (reverse animations))))
-
 (defn compute-frames [app-db id forward? animations]
-  (let [current-state (vec (get-in app-db [:kv id-key id]))
+  (let [current-state (vec (get-in app-db [:kv id-key id :values]))
+        prev-forward? (get-in current-state [0 :keechma.toolbox/anim-state :forward?])
+        same-direction? (or (nil? prev-forward?) (= forward? prev-forward?))
+        current-state (vec (if same-direction? current-state (reverse current-state)))
         animations-with-frames
         (vec (map-indexed (fn [idx animation]
                             (assoc animation :frames (calculate-frames animation forward? (get current-state idx))))
-                          animations))
-
-        max-frames-count (apply max (map #(count (:frames %)) animations-with-frames))]
+                          (if forward? animations (reverse animations))))]
     (-> animations-with-frames
-        (add-delay forward? max-frames-count)
+        (add-delay forward?)
         (normalize-frames forward?)
-        (reverse-animations forward?))))
+        )))
 
 (defn get-values-for-frame [animations forward? frame]
   (mapv (fn [animation]
@@ -86,26 +131,38 @@
                     [:keechma.toolbox/anim-state :forward?] forward?))
         animations))
 
+(def version-counter (atom 0))
+
+(defn next-version! []
+  (swap! version-counter #(+ 0.00001 %))
+  @version-counter)
+
 (defn play-animation! [app-db id forward? animations]
   (let [normalized (compute-frames app-db id forward? animations)
-        final-frame-count (count (:frames (first normalized)))]
+        final-frame-count (count (:frames (first normalized)))
+        version (next-version!)
+        commit-frame (fn [i]
+                       (fn [value app-db]
+                         (when (>= version (get-in app-db [:kv id-key id :version]))
+                           (pp/commit! (assoc-in app-db [:kv id-key id]
+                                                 {:version version
+                                                  :values (get-values-for-frame normalized forward? i)})))))
+        check-version! (fn [value app-db]
+                         (when (not= version (get-in app-db [:kv id-key id :version]))
+                           (throw (ex-info "New animation version" {:new-animation-version? true}))))]
     (pp/make-pipeline
      {:begin (reduce (fn [acc i]
                        (if (= 0 i)
-                         (conj acc
-                               (fn [value app-db]
-                                 (pp/commit! (assoc-in app-db [:kv id-key id]
-                                                       (get-values-for-frame normalized forward? i)))))
-                         (concat acc
-                                 [raf-promise
-                                  (fn [value app-db]
-                                    (pp/commit! (assoc-in app-db [:kv id-key id]
-                                                          (get-values-for-frame normalized forward? i))))])))
-                     [] (range 0 final-frame-count))})))
+                         (conj acc (commit-frame i))
+                         (concat acc [check-version! raf-promise check-version! (commit-frame i)])))
+                     [] (range 0 final-frame-count))
+      :rescue [(fn [value app-db error]
+                 (when-not (= {:new-animation-version? true} (.-data (:payload error)))
+                   (throw error)))]})))
 
 (defn render-first-frame [app-db id forward? animations]
   (let [normalized (compute-frames app-db id forward? animations)]
-    (assoc-in app-db [:kv id-key id] (get-values-for-frame normalized forward? 0))))
+    (assoc-in app-db [:kv id-key id :values] (get-values-for-frame normalized forward? 0))))
 
 (def animations
   [{:values {:margin-left [0 200]
@@ -114,8 +171,9 @@
              :rotation [0 360]
              :width [40 40]}
     :calculator calc}
-   {:values {:width [40 200]}
-    :delay 50
+   {:values {:width [40 200]
+             :rotation [360 360]}
+    :delay 100
     :calculator calc}
    {:values {:width [200 40]
              :margin-left [200 360]}
@@ -128,8 +186,12 @@
      true)
    {:start (pipeline! [value app-db]
              (pp/commit! (render-first-frame app-db :animation true animations)))
-    :animation (pp/exclusive (pipeline! [value app-db]
-                               (play-animation! app-db :animation (= :forward value) animations)))}))
+    :restart (pipeline! [value app-db]
+               (println "TEST")
+               (pp/commit! (assoc-in app-db [:kv id-key] nil))
+               (pp/commit! (render-first-frame app-db :animation true animations)))
+    :animation (pipeline! [value app-db]
+                 (play-animation! app-db :animation (= :forward value) animations))}))
 
 (def controllers
   {:anim anim-controller})
--- src/cljs/animation_keechma/ui/main.cljs
+++ src/cljs/animation_keechma/ui/main.cljs
@@ -3,37 +3,23 @@
             [keechma.toolbox.ui :refer [sub> <cmd]]))
 
 (defn merge-animation-values [state]
-  (:values (reduce (fn [acc frame]
-                     (let [placeholder? (or (get-in frame [:keechma.toolbox/anim-state :placeholder?]) false)
-                           values (:values frame)]
-                       (reduce-kv (fn [m k v]
-                                    (let [value-exists? (contains? (:values m) k)
-                                          value-placeholder? (get-in m [:placeholders k])]
-                                      (cond
-                                        (not value-exists?)
-                                        (-> m
-                                            (assoc-in [:values k] v)
-                                            (assoc-in [:placeholders k] placeholder?))
-
-                                        (and value-exists? value-placeholder? placeholder?)
-                                        (-> m
-                                            (assoc-in [:values k] v)
-                                            (assoc-in [:placeholders k] placeholder?))
-
-                                        (and value-exists? value-placeholder? (not placeholder?))
-                                        (-> m
-                                            (assoc-in [:values k] v)
-                                            (assoc-in [:placeholders k] placeholder?))
-
-                                        (and value-exists? (not value-placeholder?) (not placeholder?))
-                                        (-> m
-                                            (assoc-in [:values k] v)
-                                            (assoc-in [:placeholders k] placeholder?))
-                                        
-                                        :else m))) acc values)))
-                   {:values {} :placeholders {}} state))
-  ;;(apply merge (map :values state))
-  )
+  (:values (reduce 
+            (fn [acc frame]
+              (let [placeholder? (or (get-in frame [:keechma.toolbox/anim-state :placeholder?]) false)
+                    values (:values frame)]
+                (reduce-kv (fn [m k v]
+                             (let [value-exists? (contains? (:values m) k)
+                                   value-placeholder? (get-in m [:placeholders k])]
+                               (if (or (not value-exists?)
+                                       (and value-exists? value-placeholder? placeholder?)
+                                       (and value-exists? value-placeholder? (not placeholder?))
+                                       (and value-exists? (not value-placeholder?) (not placeholder?)))
+                                 (-> m
+                                     (assoc-in [:values k] v)
+                                     (assoc-in [:placeholders k] placeholder?))
+                                 m)
+                               )) acc values)))
+            {:values {} :placeholders {}} (:values state))))
 
 (defn render [ctx]
   (let [anim-state (merge-animation-values (sub> ctx :anim-state))
@@ -41,6 +27,7 @@
     [:div
      [:button {:on-mouse-down #(<cmd ctx :animation :forward)
                :on-mouse-up #(<cmd ctx :animation :back)} "Animate"]
+     [:button {:on-click #(<cmd ctx :restart true)} "Restart"]
      [:hr]
      (when anim-state
        [:div {:style {:width (str (:width anim-state) "px")
@@ -50,7 +37,7 @@
                       :border-radius (str (:border-radius anim-state) "px")
                       :transform (str "rotate(" (:rotation anim-state) "deg)")
                       }}
-       ])]))
+        ])]))
 
 (def component (ui/constructor {:renderer render
                                 :subscription-deps [:anim-state]
