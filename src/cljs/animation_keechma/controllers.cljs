7c2dcedb1f3588d05fe45ce65dc5912e29a7560b
Mihael Konjevic <konjevic@gmail.com>
2017-10-25 01:11:31 +0200

    Cleanup

--- src/cljs/animation_keechma/controllers.cljs
+++ src/cljs/animation_keechma/controllers.cljs
@@ -13,183 +13,9 @@
                                              non-blocking-animation!
                                              stop-task!
                                              cancel-task!]]
-            [animation-keechma.ui.main :as ui-main :refer [animation]]
+            [animation-keechma.ui.main :as ui-main]
             [animation-keechma.animation.animator :as animator]))
 
-
-#_(def anim-controller
-  (pp-controller/constructor
-   (fn [] true)
-   {:start (pipeline! [value app-db]
-             (pp/commit! (cancel-task! app-db :animate-button))
-             (pp/commit! (cancel-task! app-db :window-resize))
-
-             (pp/commit! (assoc-in app-db [:kv :animation]
-                                   {:id :button-start
-                                    :position 0
-                                    :value start-styles}))
-             (pp/execute! :measure-screen nil)
-             #_(non-blocking-task! window-mouse-move-runner :mouse-move
-                                 (fn [{:keys [value]} app-db]
-                                   (println value)
-                                   app-db))
-             (rescue! [error]
-               (println error)))
-    :measure-screen (pipeline! [value app-db]
-                      #_(blocking-task! window-resize-runner :window-resize
-                                      (fn [{:keys [value]} app-db]
-                                        (println value)
-                                        app-db))
-                      ;;(println "I'M BLOCKED BY THE WINDOW MEASURER")
-                      (rescue! [error]
-                        (println error)))
-    :stop (pipeline! [value app-db]
-            (pp/commit! (cancel-task! app-db :animate-button))
-            (pp/commit! (cancel-task! app-db :window-resize))
-            (pp/commit! (cancel-task! app-db :mouse-move))
-            (rescue! [error]
-              (println error)))
-    :animate-press (pipeline! [value app-db]
-                     (pp/commit! (cancel-task! app-db :animate-button))
-                     (blocking-animation! :animate-button (animate-press))
-                     (rescue! [error]
-                       (println error)))
-    :animate-submit (pipeline! [value app-db]
-                      (pp/commit! (stop-task! app-db :animate-button))
-                      (blocking-animation! :animate-button (animate-release app-db))
-                      (blocking-animation! :animate-button (animate-button-to-loader app-db))
-                      (pp/commit! (assoc-in app-db [:kv :animation] 
-                                            {:id :spinner-start
-                                             :position 0
-                                             :value loader-styles}))
-                      (blocking-animation! :animate-button (animate-loader-to-spinner app-db))
-                      (non-blocking-animation! :animate-button
-                                          (fn [{:keys [times-invoked]} app-db]
-                                            (update-in app-db [:kv :animation :value :rotation] inc)))
-                      (delay-pipeline (* 3 (rand-int 500)))
-                      (pp/execute! :finish-loading true)
-                      (rescue! [error]
-                        (println error)))
-    :finish-loading (pipeline! [value app-db]
-                      (pp/commit! (stop-task! app-db :animate-button))
-                      (blocking-animation! :animate-button (animate-spinner-to-button app-db))
-                      (pp/commit! (assoc-in app-db [:kv :animation]
-                                            {:id :button-start
-                                             :position 0
-                                             :value success-start-styles}))
-                      (blocking-animation! :animate-button (animate-success app-db))
-                      (rescue! [error]
-                        (println error)))
-    :cancel (pipeline! [value app-db]
-              (println "CANCEL")
-              (pp/commit! (stop-task! app-db :mouse-move))
-              (pp/commit! (stop-task! app-db :window-resize)))}))
-
-
-;; (defn animate-press []
-;;   (let [s (Spring. 0.9 800 40)
-;;         se (start-end-styles start-styles press-down-styles)]
-;;     (.snap s 0)
-;;     (.setEnd s 1)
-;;     (fn [{:keys [id]} app-db]
-;;       (let [value (.x s)
-;;             next-app-db (-> app-db
-;;                             (assoc-in [:kv :animation :value] (get-current-styles value se))
-;;                             (assoc-in [:kv :animation :position] value))]
-;;         (if (not (.done s))
-;;           next-app-db
-;;           (stop-task! next-app-db id))))))
-
-;; (defn animate-release [app-db]
-;;   (let [s (Spring. 0.9 800 40)
-;;         current (get-in app-db [:kv :animation])
-;;         current-styles (or (:value current) start-styles)
-;;         se (start-end-styles current-styles start-styles)
-;;         position (or (- 1 (:position current)) 0)]
-;;     (.snap s position)
-;;     (.setEnd s 1)
-;;     (fn [{:keys [id]} app-db]
-;;       (let [value (.x s)
-;;             next-app-db (-> app-db
-;;                             (assoc-in [:kv :animation :value] (get-current-styles value se))
-;;                             (assoc-in [:kv :animation :position] value))]
-;;         (if (or (.done s)
-;;                 (> value 1.02))
-;;           (stop-task! next-app-db id)
-;;           next-app-db)))))
-
-;; (defn animate-button-to-loader [app-db]
-;;   (let [s (Spring. 1 400 20)
-;;         se (start-end-styles start-styles button->loader-styles)]
-;;     (.snap s 0)
-;;     (.setEnd s 1)
-;;     (fn [{:keys [id]} app-db]
-;;       (let [value (.x s)
-;;             next-app-db (-> app-db
-;;                             (assoc-in [:kv :animation :value] (get-current-styles value se))
-;;                             (assoc-in [:kv :animation :position] value))]
-;;         (if (or (.done s)
-;;                 (> value 1))
-;;           (stop-task! next-app-db id)
-;;           next-app-db
-;;           )))))
-
-;; (defn animate-loader-to-spinner [app-db]
-;;   (let [s (Spring. 1 800 20)
-;;         se (start-end-styles loader-styles loader-starting-styles)]
-;;     (.snap s 0)
-;;     (.setEnd s 1)
-;;     (fn [{:keys [times-invoked id]} app-db]
-;;       (let [value (.x s)
-;;             next-app-db (-> app-db
-;;                             (assoc-in [:kv :animation :value] (get-current-styles value se))
-;;                             (assoc-in [:kv :animation :position] value)
-;;                             (assoc-in [:kv :animation :value :rotation] times-invoked))]
-;;         (if (.done s)
-;;           (stop-task! next-app-db id)
-;;           next-app-db
-;;           )))))
-
-;; (defn animate-spinner-to-button [app-db]
-;;   (let [s (Spring. 1 400 40)
-;;         current (get-in app-db [:kv :animation])
-;;         current-styles (or (:value current) loader-starting-styles)
-;;         se (start-end-styles current-styles loader-button-styles)
-;;         position (or (- 1 (:position current)) 0)]
-;;     (.snap s position)
-;;     (.setEnd s 1)
-;;     (fn [{:keys [id]} app-db]
-;;       (let [value (.x s)
-;;             next-app-db (-> app-db
-;;                             (assoc-in [:kv :animation :value] (get-current-styles value se))
-;;                             (assoc-in [:kv :animation :position] value))]
-;;         (if (not (.done s))
-;;           next-app-db
-;;           (stop-task! next-app-db id))))))
-
-;; (defn animate-success [app-db]
-;;   (let [s (Spring. 1 400 20)
-;;         se (start-end-styles success-start-styles success-end-styles)]
-;;     (.snap s 0)
-;;     (.setEnd s 1)
-;;     (fn [{:keys [times-invoked id]} app-db]
-;;       (let [value (.x s)
-;;             next-app-db (-> app-db
-;;                             (assoc-in [:kv :animation :value] (get-current-styles value se))
-;;                             (assoc-in [:kv :animation :position] value)
-;;                             (assoc-in [:kv :animation :value :rotation] times-invoked))]
-;;         (if (not (.done s))
-;;           next-app-db
-;;           (stop-task! next-app-db id))))))
-
-
-(def Spring (.-Spring gravitas))
-
-
-
-
-
-(defn noop [& args])
 
 (defn delay-pipeline [msec]
   (p/promise (fn [resolve reject]
@@ -200,7 +26,7 @@
   ([[id state] prev]
    {:id id :state state :position 0 :times-invoked 0 :prev nil}))
 
-(defn render-animation-state [app-db identifier]
+(defn render-animation-end [app-db identifier]
   (let [[id _] identifier
         init-meta (make-initial-meta identifier)]
     (assoc-in app-db [:kv :animations id]
@@ -247,7 +73,7 @@
    {:start (pipeline! [value app-db]
              (println "STARTING")
              (stop-task! app-db :button)
-             (pp/commit! (render-animation-state app-db [:button :init]))
+             (pp/commit! (render-animation-end app-db [:button :init]))
              (rescue! [error]
                (ignore-task-cancellation error)))
     :animate-press (pipeline! [value app-db]
@@ -261,18 +87,18 @@
                        (blocking-animate-state! app-db [:button :init])
                        (blocking-animate-state! app-db [:button :fail-init]))
                     (blocking-animate-state! app-db [:button :button-loader])
-                    (pp/commit! (render-animation-state app-db [:button :loader]))
+                    (pp/commit! (render-animation-end app-db [:button :loader]))
                     (non-blocking-animate-state! app-db [:button :loader])
                     (delay-pipeline 1500)
                     (when (get-in app-db [:kv :should-fail?])
                       (throw (ex-info "BLA BLA" {})))
                     (stop-task! app-db :button)
-                    (pp/commit! (render-animation-state app-db [:button :button-loader]))
+                    (pp/commit! (render-animation-end app-db [:button :button-loader]))
                     (blocking-animate-state! app-db [:button :success-notice])
                     (blocking-animate-state! app-db [:button :init])
                     (rescue! [error]
                       (ignore-task-cancellation error)
-                      (pp/commit! (render-animation-state app-db [:button :button-loader]))
+                      (pp/commit! (render-animation-end app-db [:button :button-loader]))
                       (blocking-animate-state! app-db [:button :fail-notice])
                       (blocking-animate-state! app-db [:button :fail-init])
                                           
--- src/cljs/animation_keechma/ui/main.cljs
+++ src/cljs/animation_keechma/ui/main.cljs
@@ -8,8 +8,6 @@
 
 (def Spring (.-Spring gravitas))
 
-
-
 (extend-type Spring
   animator/IAnimator
   (position [this]
@@ -59,100 +57,6 @@
 (defmethod values :default [meta]
   {})
 
-#_(case id
-    :button-start [:button {:style {:border-radius "25px"
-                                    :height (str (:height value) "px")
-                                    :border-style "solid"
-                                    :border-width (str (:border-width value) "px")
-                                    :outline "none"
-                                    :cursor "pointer"
-                                    :overflow "hidden"
-                                    :color (:color value)
-                                    :font-size (str (:font-size value) "px")
-                                    :border-color (:border-color value)
-                                    :background (:background-color value)
-                                    :width (str (:width value) "px")
-                                    :margin-left (str (:margin-left value) "px")
-                                    :margin-top (str (:margin-top value) "px")
-                                    :padding 0
-                                    :position "absolute"
-                                    :-webkit-tap-highlight-color "rgba(0,0,0,0)"}
-                            :on-mouse-down #(<cmd ctx :animate-press)
-                            :on-click #(<cmd ctx :animate-submit)
-                            }
-                   [:span {:style {:opacity (:text-opacity value)}} "Submit"]
-                   [:div {:style {:position "absolute"
-                                  :border-radius "100%"
-                                  :top (str (:checkmark-pos value) "px")
-                                  :left "50%"
-                                  :width (str (:checkmark-dim value) "px")
-                                  :height (str (:checkmark-dim value) "px")
-                                  :line-height "20px"
-                                  :text-align "center"
-                                  :overflow "hidden"
-                                  :color "white"
-                                  :opacity (:checkmark-opacity value)
-                                  :margin-left (str "-" (/ (:checkmark-dim value) 2) "px")
-                                  }} "✔"]]
-    :spinner-start [:div {:style {:width (str (:width value) "px")
-                                  :height "50px"
-                                  :border-radius "25px"
-                                  :background-color (:background-color value)
-                                  :margin-left (:margin-left value)
-                                  :overflow "hidden"
-                                  :position "relative"}}
-                    [:div {:style {:background "#03cd94"
-                                   :position "absolute"
-                                   :width "50px"
-                                   :height "50px"
-                                   :left "-25px"
-                                   :top "-25px"
-                                   :opacity (:spinner-opacity value)
-                                   :transform (str "rotate(" (* 6 (:rotation value)) "deg)")
-                                   :transform-origin "100% 100%"
-                                   }}]
-                    [:div {:style {:position "absolute"
-                                   :width (str (- (:width value) (- 50 (:inner-circle-dim value))) "px")
-                                   :height (str (:inner-circle-dim value) "px")
-                                   :left (str (:inner-circle-pos value) "px")
-                                   :top (str (:inner-circle-pos value) "px")
-                                   :background "white"
-                                   :border-radius (str (/ (:inner-circle-dim value) 2) "px")
-                                   :opacity (:inner-circle-opacity value)
-                                   }}]
-                    [:div {:style {:position "absolute"
-                                   :border-radius "100%"
-                                   :top (str (:checkmark-pos value) "px")
-                                   :left "50%"
-                                   :width (str (:checkmark-dim value) "px")
-                                   :height (str (:checkmark-dim value) "px")
-                                   :line-height "20px"
-                                   :text-align "center"
-                                   :overflow "hidden"
-                                   :color "white"
-                                   :margin-left (str "-" (/ (:checkmark-dim value) 2) "px")
-                                   }} "✔"]]
-    nil)
-
-
-
-#_(def animation
-  {:states {'init           {:press 'pressed}
-            'pressed        {:release 'button-loader}
-            'button-loader  {:next 'loader}
-            'loader         {:success 'success-notice
-                             :fail    'fail-notice}
-            'success-notice {:next 'init}
-            'fail-notice    {:next 'fail-init}
-            'fail-init      {:press 'fail-pressed}
-            'fail-pressed   {:release 'button-loader}}
-   })
-
-
-
-
-
-
 (defmethod values [:button :init] [_]
   {:border-radius    "25px"
    :height           "50px"
@@ -175,7 +79,6 @@
 (defmethod done? [:button :init] [meta animator]
   (or (animator/done? animator)
       (> (:position meta) 1.02)))
-
 
 (defmethod values [:button :pressed] [_]
   {:border-radius    "25px"
@@ -301,28 +204,6 @@
   (or (animator/done? animator)
       (> (:position meta) 1.02)))
 
-(def animation
-  {:id     :button
-   :states {:init           {:style {}}
-            :pressed        {:style {}}
-            :button-loader  {:style {}}
-            :loader         {:style {:border-radius   "25px"
-                                     :height          "50px"
-                                     :width           "50px"
-                                     :display         "flex"
-                                     :background      "#ccc"
-                                     :overflow        "hidden"
-                                     :justify-content "center"
-                                     :align-items     "center"
-                                     :position        "relative"}}
-            :success-notice {:style {}}
-            :fail-notice    {:style {}}
-            :fail-init      {:style {}}
-            :fail-pressed   {:style {}}}})
-
-
-
-
 
 (defn render-button [ctx anim]
   (let [state (get-in anim [:meta :state])]
@@ -338,7 +219,7 @@
      (when (= :fail-notice state)
        [:div {:style {:opacity 1}} "✘"])]))
 
-(defn render-loader2 [ctx anim]
+(defn render-loader [ctx anim]
   [:div {:style {:border-radius "25px"
                  :height "50px"
                  :width "50px"
@@ -361,128 +242,7 @@
                   :height "42px"
                   :position "relative"}}]])
 
-(defn render-init []
-  [:button {:style {:border-radius    "25px"
-                    :height           "50px"
-                    :width            "200px"
-                    :border-width     "2px"
-                    :border-style     "solid"
-                    :border-color     "#03cd94"
-                    :color            "#07ce95"
-                    :background-color "#fff"
-                    :font-size        "16px"
-                    :cursor           "pointer"
-                    :outline          "none"}}
-   [:span {:style {:opacity 1}} "Submit"]])
 
-(defn render-pressed []
-  [:button {:style {:border-radius "25px"
-                    :height "44px"
-                    :width "180px"
-                    :border-width "2px"
-                    :border-style "solid"
-                    :border-color "#03cd94"
-                    :color "#fff"
-                    :background-color "#03cd94"
-                    :font-size "14px"
-                    :cursor "pointer"
-                    :outline "none"}}
-   [:span {:style {:opacity 1}} "Submit"]])
-
-(defn render-button-loader []
-  [:button {:style {:border-radius "25px"
-                    :height "50px"
-                    :width "50px"
-                    :border-width "4px"
-                    :border-style "solid"
-                    :border-color "#ccc"
-                    :color "#fff"
-                    :background-color "#fff"
-                    :font-size "14px"
-                    :cursor "pointer"
-                    :outline "none"}}
-   [:span {:style {:opacity 0}} "Submit"]])
-
-(defn render-loader []
-  [:div {:style {:border-radius "25px"
-                 :height "50px"
-                 :width "50px"
-                 :display "flex"
-                 :background "#ccc"
-                 :overflow "hidden"
-                 :justify-content "center"
-                 :align-items "center"
-                 :position "relative"}}
-   [:div {:style {:background "#ff3300"
-                  :width "50px"
-                  :height "25px"
-                  :margin-left "-25px"
-                  :position "absolute"
-                  :transform "rotate(23deg)"
-                  :transform-origin "100% 50%"}}]
-   [:div {:style {:border-radius "25px"
-                  :background-color "white"
-                  :width "42px"
-                  :height "42px"
-                  :position "relative"}}]])
-
-(defn render-button-success []
-  [:button {:style {:border-radius "25px"
-                    :height "50px"
-                    :width "50px"
-                    :border-width "4px"
-                    :border-style "solid"
-                    :border-color "#03cd94"
-                    :color "#fff"
-                    :background-color "#03cd94"
-                    :font-size "14px"
-                    :cursor "pointer"
-                    :outline "none"}}
-   [:span {:style {:opacity 1}} "✔"]])
-
-
-
-(defn render-button-fail []
-  [:button {:style {:border-radius "25px"
-                    :height "50px"
-                    :width "50px"
-                    :border-width "4px"
-                    :border-style "solid"
-                    :border-color "#ff3300"
-                    :color "#fff"
-                    :background-color "#ff3300"
-                    :font-size "14px"
-                    :cursor "pointer"
-                    :outline "none"}}
-   [:span {:style {:opacity 1}} "✘"]])
-
-(defn render-fail-init []
-  [:button {:style {:border-radius "25px"
-                    :height "50px"
-                    :width "200px"
-                    :border-width "2px"
-                    :border-style "solid"
-                    :border-color "#ff3300"
-                    :color "#ff3300"
-                    :background-color "#fff"
-                    :font-size "16px"
-                    :cursor "pointer"
-                    :outline "none"}}
-   [:span {:style {:opacity 1}} "Submit Failed"]])
-
-(defn render-fail-pressed []
-  [:button {:style {:border-radius "25px"
-                    :height "44px"
-                    :width "180px"
-                    :border-width "2px"
-                    :border-style "solid"
-                    :border-color "#ff3300"
-                    :color "#fff"
-                    :background-color "#ff3300"
-                    :font-size "14px"
-                    :cursor "pointer"
-                    :outline "none"}}
-   [:span {:style {:opacity 1}} "Submit"]])
 
 (defn render [ctx]
   (let [anim (sub> ctx :animation)
@@ -497,15 +257,7 @@
        [:input {:type :checkbox :checked (sub> ctx :should-fail?) :on-change #(<cmd ctx :toggle-should-fail)}]
        " Req should fail?"]]
      [:hr]
-     (into [:div]
-           (map (fn [renderer]
-                  [:div {:style {:display "flex"
-                                 :justify-content "center"
-                                 :align-items "center"
-                                 :height "100px"
-                                 :border "1px solid black"
-                                 :margin-bottom "-1px"}}
-                   [renderer]]) [render-init render-pressed render-button-loader render-loader render-button-success render-button-fail render-fail-init render-fail-pressed]))
+     
      [:div {:style {:display "flex"
                     :justify-content "center"
                     :align-items "center"
@@ -515,7 +267,7 @@
       (when (contains? #{:init :pressed :button-loader :success-notice :fail-notice :fail-init :fail-pressed} anim-state)
         [render-button ctx anim])
       (when (contains? #{:loader} anim-state)
-        [render-loader2 ctx anim])]]))
+        [render-loader ctx anim])]]))
 
 (def component (ui/constructor {:renderer render
                                 :subscription-deps [:anim-state :animation :should-fail?]
